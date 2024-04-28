98819d89ba4f992fbfd344a07bc4b0318a03a17c
Jason Chen <jinyuche@amazon.com>
2024-04-28 16:46:51 -0700

    1. Fixed a bug in the cdk.json 2. Added APIGW & Lambda

--- lib/fovus-coding-game-stack.ts
+++ lib/fovus-coding-game-stack.ts
@@ -1,11 +1,34 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import { aws_s3 as s3 } from 'aws-cdk-lib';
+import { Function, Runtime, Code } from 'aws-cdk-lib/aws-lambda';
+import {  HttpLambdaIntegration } from 'aws-cdk-lib/aws-apigatewayv2-integrations';
+import { HttpApi, HttpMethod } from 'aws-cdk-lib/aws-apigatewayv2'
+
+
 export class FovusCodingGameStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
-    new s3.Bucket(this, 'emma_jiang_test_stack', {
+    const fileStorageBucket = new s3.Bucket(this, 'file-storage-bucket', {
       versioned: true
     })
+    // Create Lambda function
+    const requestProcessor = new Function(this, 'requestProcessor', {
+      functionName: 'MyLambdaFunction',
+      runtime: Runtime.NODEJS_20_X,
+      handler: 'request-process.handler',
+      code: Code.fromAsset('lib/lambda'),
+    });
+    const requestProcessorIntegration = new HttpLambdaIntegration('fileUploadAPI', requestProcessor);
+
+    const httpApi = new HttpApi(this, 'HttpApi', {
+      apiName: `FileUploadAPI`,
+      createDefaultStage: true,
+  });
+    httpApi.addRoutes({
+      path: '/hello',
+      methods: [ HttpMethod.GET ],
+      integration: requestProcessorIntegration,
+    });
   }
 }

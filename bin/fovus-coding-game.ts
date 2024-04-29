c277d21c6086ab98f605dd51d4172fc0a85f4f11
Jason Chen <jinyuche@amazon.com>
2024-04-28 22:29:53 -0700

    1. Added AWS SDK as dependency 2. Added sample lambda code with S3 putObject

--- bin/fovus-coding-game.ts
+++ bin/fovus-coding-game.ts
@@ -4,18 +4,8 @@ import * as cdk from 'aws-cdk-lib';
 import { FovusCodingGameStack } from '../lib/fovus-coding-game-stack';
 
 const app = new cdk.App();
+// AWS region to deploy your stack into, defaulting to us-west-2 because of location proximity
+const deploymentRegion = 'us-west-2'
 new FovusCodingGameStack(app, 'FovusCodingGameStack', {
-  /* If you don't specify 'env', this stack will be environment-agnostic.
-   * Account/Region-dependent features and context lookups will not work,
-   * but a single synthesized template can be deployed anywhere. */
-
-  /* Uncomment the next line to specialize this stack for the AWS Account
-   * and Region that are implied by the current CLI configuration. */
-  // env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
-
-  /* Uncomment the next line if you know exactly what Account and Region you
-   * want to deploy the stack to. */
-  // env: { account: '123456789012', region: 'us-east-1' },
-
-  /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
+  env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: deploymentRegion },
 });
--- lib/lambda/request-process.ts
+++ lib/lambda/request-process.ts
@@ -1,11 +1,40 @@
-
+import * as s3Client from "aws-sdk/clients/s3";
+import * as dynamoClient from "aws-sdk/clients/dynamodb";
+import { nanoid } from 'nanoid'
+import { AWSError } from "aws-sdk";
+import { PromiseResult } from "aws-sdk/lib/request";
 
 exports.handler = async (event:any) => {
     // Process the event received:
     console.log(event);
+    const idLength = 10;
+    //TODO: parameterize this via env var
+    const bucketName = "emma-jiang-fovus-coding-game-file-storage-bucket";
+    // Generate the ID, lenght is set to 10
+    let id = nanoid(idLength);
+
+    // upload the file to S3
+    await putObject(new s3Client(), "emma-jiang-fovus-coding-game-file-storage-bucket", id, event.body);
+    new dynamoClient().putItem(); // upload item to dynamodb;
     return {
         statusCode: 200,
         headers: { "Content-Type": "text/plain" },
         body: JSON.stringify({ message: "Hello, World!" }),
     };
 };
+
+
+// Use this helper function to upload an object to S3 bukect
+export async function putObject(s3Client: s3Client, bucketName: string, keyName: string, bodyContent: any): Promise <PromiseResult <void, AWSError>> {
+    let params = {
+        Bucket: bucketName,
+        Key: keyName,
+        Body: bodyContent
+    }
+    try {
+        await s3Client.putObject(params).promise();
+    } catch (error) {
+        console.log(`There was an error when trying to put object to S3: ${JSON.stringify(error)}`)
+        throw error;
+    }
+}

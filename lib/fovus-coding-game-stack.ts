9a731724b19fb1095a377eb6cf339fe61eacad04
Jason Chen <jinyuche@amazon.com>
2024-04-28 17:41:43 -0700

    1. Add the DynamoDB table 2. Formatting

--- lib/fovus-coding-game-stack.ts
+++ lib/fovus-coding-game-stack.ts
@@ -1,34 +1,57 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-import { aws_s3 as s3 } from 'aws-cdk-lib';
+import { aws_s3 as s3, aws_dynamodb as dynamodb} from 'aws-cdk-lib';
 import { Function, Runtime, Code } from 'aws-cdk-lib/aws-lambda';
 import {  HttpLambdaIntegration } from 'aws-cdk-lib/aws-apigatewayv2-integrations';
-import { HttpApi, HttpMethod } from 'aws-cdk-lib/aws-apigatewayv2'
+import { HttpApi, HttpMethod, HttpStage } from 'aws-cdk-lib/aws-apigatewayv2'
 
 
 export class FovusCodingGameStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
+    /************/
+    /*    S3    */
+    /************/
     const fileStorageBucket = new s3.Bucket(this, 'file-storage-bucket', {
-      versioned: true
+      bucketName: "emma-jiang-fovus-coding-game-file-storage-bucket",
+      blockPublicAccess: s3.BlockPublicAccess.BLOCK_ALL // block public access
     })
-    // Create Lambda function
+
+    /************/
+    /*  LAMBDA  */
+    /************/
     const requestProcessor = new Function(this, 'requestProcessor', {
-      functionName: 'MyLambdaFunction',
+      functionName: 'RequestProcessorLambda',
       runtime: Runtime.NODEJS_20_X,
       handler: 'request-process.handler',
       code: Code.fromAsset('lib/lambda'),
     });
+    requestProcessor.addEnvironment("EXAMPLE_KEY", "EXAMPLE_VALUE");
+    /************/
+    /*  APIGW   */
+    /************/
     const requestProcessorIntegration = new HttpLambdaIntegration('fileUploadAPI', requestProcessor);
-
-    const httpApi = new HttpApi(this, 'HttpApi', {
+    const fileUploadAPI = new HttpApi(this, 'HttpApi', {
       apiName: `FileUploadAPI`,
       createDefaultStage: true,
-  });
-    httpApi.addRoutes({
+    });
+    new HttpStage(this, 'Stage', {
+      httpApi: fileUploadAPI,
+      stageName: 'beta',
+    });
+    fileUploadAPI.addRoutes({
       path: '/hello',
-      methods: [ HttpMethod.GET ],
+      methods: [ HttpMethod.POST ],
       integration: requestProcessorIntegration,
     });
+
+    /************/
+    /* DynamoDB */
+    /************/
+    const table = new dynamodb.Table(this, 'UploadedFile', {
+      partitionKey: { name: 'id', type: dynamodb.AttributeType.STRING },
+      stream: dynamodb.StreamViewType.NEW_IMAGE,
+    });
+
   }
 }
--- lib/lambda/request-process.ts
+++ lib/lambda/request-process.ts
@@ -1,4 +1,8 @@
+
+
 exports.handler = async (event:any) => {
+    // Process the event received:
+    console.log(event);
     return {
         statusCode: 200,
         headers: { "Content-Type": "text/plain" },
